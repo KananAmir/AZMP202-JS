9086bccf79612809d91d4fa2aeca0ba181f24e64
KananAmir <amirovknn@gmail.com>
2024-11-25 16:47:33 +0400

    json-server

--- 11.25.js-axios.async-await/assets/js/contants.js
+++ 11.25.js-axios.async-await/assets/js/contants.js
@@ -1,4 +1,5 @@
-const BASE_URL = "http://localhost:8080";
+// const BASE_URL = "http://localhost:8080";
+const BASE_URL = "https://azmp202-json-server-deployment.vercel.app";
 
 const endpoints = {
   products: "products",
